2f49712ef2f67c0613b339315fc961039b4969c0
LimJaeEun0930 <je9513666@gmail.com>
2024-12-02 14:33:55 +0900

    feat: 사용자입력에 따른 건널수있는 다리 설정( UDDUUD 이런식..)

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,6 +19,19 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> steppingStones = new ArrayList<>(3);
+        for (int i = 0; i < size; ++i) {
+            addRandomValudeToList(steppingStones, bridgeNumberGenerator.generate());
+        }
+        return steppingStones;
+    }
+
+    private void addRandomValudeToList(List<String> steppingStones, int randomValue) {
+        if (randomValue == 1) {
+            steppingStones.add("U");
+        }
+        if (randomValue == 0) {
+            steppingStones.add("D");
+        }
     }
 }
